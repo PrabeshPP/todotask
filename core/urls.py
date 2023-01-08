79148fba045bcbeeda82606590ebf804b2e3a90a
PrabeshPP <prabesh.bista369@gmail.com>
2023-01-07 23:31:45 -0500

    implemented login

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import getHome
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    path("",getHome,name='home')
+    path("",getHome,name='home'),
+    path('login/',LoginView.as_view(),name='login')
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Task
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def getHome(request):
     tasks=Task.objects.all()
     context={
